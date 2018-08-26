e0cc3fe2a4dd21f526352fffc37eadbfa2950c5e
ppma <675491258@qq.com>
2018-08-27 07:43:52 +0800

    好难

--- app/src/main/java/com/ppma/myxposed/Hook1.java
+++ app/src/main/java/com/ppma/myxposed/Hook1.java
@@ -1,16 +1,18 @@
 package com.ppma.myxposed;
 
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.FragmentActivity;
 
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -19,14 +21,13 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 
-
 public class Hook1 implements IXposedHookLoadPackage {
 
 
     public static String apkName = "com.mingrisoft_it_education";
     public static String start = "http://www.mingrisoft.com/Public/images/lv";
     public SharedPreferences m_sp;
-    public String m_user_id;
+    public String m_user_id = "";
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
@@ -40,7 +41,7 @@ public class Hook1 implements IXposedHookLoadPackage {
                 if (param.hasThrowable()) return;
                 Class<?> cls = (Class<?>) param.getResult();
                 String cls_Name = cls.getName();
-                if(cls_Name.startsWith(apkName)) util("当前Class", cls_Name);
+                if (cls_Name.startsWith(apkName)) util("当前Class", cls_Name);
 
                 if (cls_Name.equals("com.mingrisoft_it_education.VideoMedia.VideoMediaPlayPage")) {
                     XposedHelpers.findAndHookMethod(cls_Name, cls.getClassLoader(), "initData", Bundle.class, new XC_MethodHook() {
@@ -51,60 +52,43 @@ public class Hook1 implements IXposedHookLoadPackage {
                             //spLogin
                             Object object = param.thisObject;
                             Class page = object.getClass();
-                            Method method = page.getMethod("getSharedPreferences",new Class[]{String.class,int.class});
-                            m_sp = (SharedPreferences) method.invoke(object,new Object[]{"login",0});
+                            Method method = page.getMethod("getSharedPreferences", new Class[]{String.class, int.class});
+                            m_sp = (SharedPreferences) method.invoke(object, new Object[]{"login", 0});
                             m_user_id = m_sp.getString("user_id", "");
-                            util("m_sp",m_user_id);
+                            util("m_sp", m_user_id);
                             m_sp.edit().putString("user_id", "106013").commit();
-/*                            Field spLogin = page.getDeclaredField("spLogin");
-                            spLogin.setAccessible(true);*/
-                            /*m_sp = (SharedPreferences) */
-/*                            Object obj_spLogin = spLogin.get(param.thisObject);
-                            Class[] classes = new Class[]{String.class,String.class};
-                            Object[] objects= new Object[]{"user_id", ""};
-                            String user_id = (String)callMethod(obj_spLogin,"getString",classes,objects);
-                            util("user_id",user_id);*/
-/*                            m_user_id = m_sp.getString("user_id", "");
-                            util("user_id",m_user_id);*/
-//                            m_sp.edit().putString("user_id", "106013").commit();
-
                         }
 
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                             super.afterHookedMethod(param);
-                            util("user_id",m_sp.getString("user_id",""));
+                            if (!m_sp.getString("user_id", "").equals("106013")) return;
+                            util("user_id", m_sp.getString("user_id", ""));
+
                             m_sp.edit().putString("user_id", m_user_id).commit();
-/*                            util("afterHookedMethod", "VideoMediaPlayPage");
-                            Class page = param.thisObject.getClass();
+                            m_user_id = m_sp.getString("user_id", "");
+                            util("afterHookedMethod", "VideoMediaPlayPage");
+                            Object object = param.thisObject;
+                            Class page = object.getClass();
                             //entityId
                             Field entityId = page.getDeclaredField("entityId");
                             entityId.setAccessible(true);
-                            //courseType
-                            Field courseType = page.getDeclaredField("courseType");
-                            courseType.setAccessible(true);
-                            //courseUrl
-                            Field courseUrl = page.getDeclaredField("courseUrl");
-                            courseUrl.setAccessible(true);
-                            courseUrl.set(param.thisObject,
-                                    "http://www.mingrisoft.com/ApiNew/getVideoInfoNew/"
-                                            + "video_id/"
-                                            + entityId.get(param.thisObject)
-                                            + "/course_type/"
-                                            + courseType.get(param.thisObject)
-                                            + "/token/qwert"
-                                            + "/user_id/"
-                                            + "106013");
-                            //courseUrl
+                            //mHandler
                             Field mHandler = page.getDeclaredField("mHandler");
                             mHandler.setAccessible(true);
                             //videoImplement
+
+                            Map hashMap2 = new HashMap();
+                            hashMap2.put("video_id", entityId.get(object));
+                            hashMap2.put("user_id", m_user_id);
                             Field videoImplement = page.getDeclaredField("videoImplement");
                             videoImplement.setAccessible(true);
-                            Object obj_videoImplement = videoImplement.get(param.thisObject);
-                            Class[] classes = new Class[]{Context.class, Handler.class, String.class, int.class};
-                            Object[] obj_togther = new Object[]{param.thisObject, mHandler.get(param.thisObject), courseUrl.get(param.thisObject), 100};
-                            callMethod(obj_videoImplement, "ObtainVideoPageData", classes, obj_togther);*/
+                            Object obj_videoImplement = videoImplement.get(object);
+                            Class[] classes = new Class[]{Context.class, Handler.class, String.class, Map.class, int.class}; // "http://www.mingrisoft.com/"++ "ApiNew/"+ "isCollectVideo/"
+                            Object[] obj_togther = new Object[]{param.thisObject, mHandler.get(param.thisObject), "http://www.mingrisoft.com/ApiNew/isCollectVideo/",hashMap2, 101};
+                            Method method = page.getDeclaredMethod("isCollectVideo",classes);
+                            method.setAccessible(true);
+                            method.invoke(object,obj_togther);
                         }
                     });
                 }
@@ -117,19 +101,13 @@ public class Hook1 implements IXposedHookLoadPackage {
                         String key = (String) param.args[0];
                         if ("picture".equals(key)) {
                             if (((String) param.getResult()).startsWith(start)) {
-                                util(key, param.getResult());
-                                XposedBridge.log("改成V2会员");
                                 param.setResult(start + "2.png");
                             }
                         }
                         if ("new_vip_name".equals(key)) {
-                            XposedBridge.log("JSONObject" + " " + key + " = " + (String) param.getResult());
-                            XposedBridge.log("改成V2会员");
                             param.setResult("V2");
                         }
                         if (key.equals("new_vip_level")) {
-                            XposedBridge.log("JSONObject" + " " + key + " = " + (String) param.getResult());
-                            XposedBridge.log("改成V2会员");
                             param.setResult("2");
                         }
 
